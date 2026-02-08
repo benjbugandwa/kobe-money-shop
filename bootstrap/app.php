c1f07507ce6793dd35bcb349fe05377c341b69c5
benjbugandwa <benjbugandwa@gmail.com>
2026-02-08 13:18:33 +0200

    Deuxieme déploiement Kobe Money Shop

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
 
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
